5effabe221fa025389813717ae49a5564dc08405
Antti Möttönen <antti.moettoenen@gmail.com>
2024-02-18 12:10:24 +0200

    Add Tailwind support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,10 @@
+import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpack
+
 plugins {
-    // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
     id("org.jetbrains.kotlin.multiplatform") version "1.9.22"
 }
 
 repositories {
-    // Use Maven Central for resolving dependencies.
     mavenCentral()
     maven {
         url = uri("https://maven.pkg.github.com/Moetto/openhue-client")
@@ -17,7 +17,13 @@ repositories {
 
 kotlin {
     js {
+        useCommonJs()
         browser {
+            commonWebpackConfig {
+                cssSupport {
+                    enabled.set(true)
+                }
+            }
         }
     }.binaries.executable()
 
@@ -26,7 +32,36 @@ kotlin {
             dependencies {
                 implementation(libs.dev.fritz2)
                 implementation(libs.dev.t3mu.openhue)
+                implementation(npm("postcss", "8.4.35"))
+                implementation(npm("postcss-loader", "8.1.0"))
+                implementation(npm("autoprefixer", "10.4.7"))
+                implementation(npm("tailwindcss", "3.4.1"))
             }
         }
+    }
+}
+
+tasks {
+    withType(KotlinWebpack::class).forEach { t ->
+        t.inputs.files(fileTree("src/jsMain/resources"))
+    }
+
+    val copyTailwindConfig by registering(Copy::class) {
+        from("./tailwind.config.js")
+        into("${rootProject.buildDir}/js/packages/${rootProject.name}-${project.name}")
+
+        dependsOn(":kotlinNpmInstall")
+    }
+
+    val copyPostcssConfig by registering(Copy::class) {
+        from("./postcss.config.js")
+        into("${rootProject.buildDir}/js/packages/${rootProject.name}-${project.name}")
+
+        dependsOn(":kotlinNpmInstall")
+    }
+
+    val jsProcessResources by getting {
+        dependsOn(copyTailwindConfig)
+        dependsOn(copyPostcssConfig)
     }
 }
--- app/src/jsMain/kotlin/dev/t3mu/huedashboard/App.kt
+++ app/src/jsMain/kotlin/dev/t3mu/huedashboard/App.kt
@@ -3,8 +3,9 @@ package dev.t3mu.huedashboard
 import dev.fritz2.core.render
 
 fun main()  {
+    js("""require("./app.css")""")
     render {
-        h1 {
+        h1("text-3xl font-bold underline") {
             +"Hello world"
         }
     }
